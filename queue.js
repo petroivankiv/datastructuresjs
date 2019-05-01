064066eed8f9a6a2fdc307b5cb85ed534fd9b332
Petro Ivankiv <petro.ivankiv88@gmail.com>
2019-05-01 19:21:35 +0300

    tided files a bit

--- queue.js
+++ queue.js
@@ -1,29 +1,29 @@
 /* Queues */
 
-function Queue () { 
+function Queue() {
     collection = [];
-    this.print = function() {
+    this.print = function () {
         console.log(collection);
     };
-    this.enqueue = function(element) {
+    this.enqueue = function (element) {
         collection.push(element);
     };
-    this.dequeue = function() {
-        return collection.shift(); 
+    this.dequeue = function () {
+        return collection.shift();
     };
-    this.front = function() {
+    this.front = function () {
         return collection[0];
     };
-    this.size = function() {
-        return collection.length; 
+    this.size = function () {
+        return collection.length;
     };
-    this.isEmpty = function() {
-        return (collection.length === 0); 
+    this.isEmpty = function () {
+        return (collection.length === 0);
     };
 }
 
-var q = new Queue(); 
-q.enqueue('a'); 
+var q = new Queue();
+q.enqueue('a');
 q.enqueue('b');
 q.enqueue('c');
 q.print();
@@ -32,45 +32,45 @@ console.log(q.front());
 q.print();
 
 
-function PriorityQueue () {
+function PriorityQueue() {
     var collection = [];
-    this.printCollection = function() {
-      (console.log(collection));
+    this.printCollection = function () {
+        (console.log(collection));
     };
-    this.enqueue = function(element){
-        if (this.isEmpty()){ 
+    this.enqueue = function (element) {
+        if (this.isEmpty()) {
             collection.push(element);
         } else {
             var added = false;
-            for (var i=0; i<collection.length; i++){
-                 if (element[1] < collection[i][1]){ //checking priorities
-                    collection.splice(i,0,element);
+            for (var i = 0; i < collection.length; i++) {
+                if (element[1] < collection[i][1]) { //checking priorities
+                    collection.splice(i, 0, element);
                     added = true;
                     break;
                 }
             }
-            if (!added){
+            if (!added) {
                 collection.push(element);
             }
         }
     };
-    this.dequeue = function() {
+    this.dequeue = function () {
         var value = collection.shift();
         return value[0];
     };
-    this.front = function() {
+    this.front = function () {
         return collection[0];
     };
-    this.size = function() {
-        return collection.length; 
+    this.size = function () {
+        return collection.length;
     };
-    this.isEmpty = function() {
-        return (collection.length === 0); 
+    this.isEmpty = function () {
+        return (collection.length === 0);
     };
 }
 
-var pq = new PriorityQueue(); 
-pq.enqueue(['Beau Carnes', 2]); 
+var pq = new PriorityQueue();
+pq.enqueue(['Beau Carnes', 2]);
 pq.enqueue(['Quincy Larson', 3]);
 pq.enqueue(['Ewa Mitulska-Wójcik', 1])
 pq.enqueue(['Briana Swift', 2])
